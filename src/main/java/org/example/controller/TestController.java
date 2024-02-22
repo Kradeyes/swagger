1b7f0c59d46665801529f32b7e33b023a4733f0c
Роман Янин <yanin.r.a@mail.ru>
2024-02-22 17:48:12 +0300

    first commit

---
+++ src/main/java/org/example/controller/TestController.java
@@ -0,0 +1,8 @@
+package org.example.controller;
+
+import org.example.swagger.controller.DefaultApi;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController implements DefaultApi {
+}
